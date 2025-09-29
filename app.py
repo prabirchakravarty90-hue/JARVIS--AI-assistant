5fd38327aee6409e362b0fa249617aded5c9606f
Prabir Chakravarty <prabirchakravarty90@gmail.com>
2025-09-29 23:40:43 +0530

    Update app.py
    Signed-off-by: Prabir Chakravarty <prabirchakravarty90@gmail.com>

--- app.py
+++ app.py
@@ -72,4 +72,4 @@ def process():
         #     return jsonify({"action": "speak", "text": "Sorry, I had trouble processing your request."})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
